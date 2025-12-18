3904991db09d604c129b1616673e8b79f09c479e
CleanIce <CleanIce2023@outlook.com>
2025-12-18 22:22:23 +0800

    Update 2025/12/18

--- src/main.rs
+++ src/main.rs
@@ -27,9 +27,12 @@ fn read_file(file_name: &str) -> String {
 }
 
 fn compile(source: &str) {
+    let lines_cnt = source.split('\n').collect::<Vec<_>>().len();
+
     let mut lexer = Lexer::new(source);
     if let Err(err) = lexer.init() {
         error::print_error(&ParseError::from(err));
+        return;
     }
     let mut parser = Parser::new(&mut lexer);
 
